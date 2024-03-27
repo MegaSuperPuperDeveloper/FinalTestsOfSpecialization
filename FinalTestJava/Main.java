bb465bc208f2f0d5cdbb6d4ca45eb79af67b5374
Krutoi <krutoi.chuvak1918@gmail.com>
2024-03-27 22:07:14 +0300

    Final Test

--- FinalTestJava/Main.java
+++ FinalTestJava/Main.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Main {
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Map<String, Integer> map = new HashMap<>();
+        System.out.println("Количество слов в файле: " + Functions.countOfWords(map));
+        System.out.println();
+        Functions.printFileData(map);;
+        System.out.println();
+        System.out.println("Cамое длинное слово: "  + Functions.longestWord(map));
+    }
+
 }
